9a8d5932f21b9a2338a25372b2e31ccd4a9f5602
Professional <Professional@USER-PC>
2024-10-09 11:20:08 +0500

    изменила название основной страницы на index.html

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
--- blog_platform/views.py
+++ blog_platform/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
 
 
